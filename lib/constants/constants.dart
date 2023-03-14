8dfdbc475014729c7506907fc4a78210586b7ffe
tomohiro1024 <gongyezhihong732@gmail.com>
2023-03-14 11:05:21 +0900

    DropdownMenuItemの作成

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 List<String> models = [
   'Model1',
   'Model2',
@@ -6,6 +8,18 @@ List<String> models = [
   'Model5',
   'Model6',
 ];
+
+List<DropdownMenuItem<String>>? get getModelsItem {
+  List<DropdownMenuItem<String>>? modelsItems =
+      List<DropdownMenuItem<String>>.generate(
+    models.length,
+    (index) => DropdownMenuItem(
+      value: models[index],
+      child: Text(models[index]),
+    ),
+  );
+  return modelsItems;
+}
 
 final chatMessages = [
   {
