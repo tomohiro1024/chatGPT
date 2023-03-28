3900f8d2d8013305dd82d370e5d7d7b55eb461cc
tomohiro1024 <gongyezhihong732@gmail.com>
2023-03-28 10:25:03 +0900

    jsonResponse['data']の取得

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 import '../constants/api_consts.dart';
+import '../models/models_model.dart';
 
 class ApiService {
-  static Future<void> getModels() async {
+  static Future<List<ModelsModel>> getModels() async {
     try {
       var response = await http.get(Uri.parse('$BASE_URL/models'),
           headers: {'Authorization': 'Bearer $API_KEY'});
@@ -14,13 +16,19 @@ class ApiService {
       Map jsonResponse = jsonDecode(response.body);
 
       if (jsonResponse['error'] != null) {
-        print('jsonResponse error: ${jsonResponse['error']['message']}');
-        print('jsonResponse error: ${jsonResponse['error']['type']}');
+        // print('jsonResponse error: ${jsonResponse['error']['message']}');
         throw HttpException(jsonResponse['error']['message']);
       }
-      print('jsonResponse: $jsonResponse');
+      // print('jsonResponse: $jsonResponse');
+      List temp = [];
+      for (var value in jsonResponse['data']) {
+        temp.add(value);
+        log('temp: $value');
+      }
+      return ModelsModel.modelsFromSnapshot(temp);
     } catch (e) {
-      print('$e');
+      log('error: $e');
+      rethrow;
     }
   }
 }
