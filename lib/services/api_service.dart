b81c5661a06a2b4e5d7edee07947e6f5dc3efbd6
tomohiro1024 <gongyezhihong732@gmail.com>
2023-03-31 18:10:59 +0900

    sendMessage()の作成開始

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -31,4 +31,29 @@ class ApiService {
       rethrow;
     }
   }
+
+  static Future<List<ModelsModel>> sendMessage(
+      {required String message, required String modelId}) async {
+    try {
+      var response = await http.post(Uri.parse('$BASE_URL/completions'),
+          headers: {'Authorization': 'Bearer $API_KEY'});
+
+      Map jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['error'] != null) {
+        // print('jsonResponse error: ${jsonResponse['error']['message']}');
+        throw HttpException(jsonResponse['error']['message']);
+      }
+      // print('jsonResponse: $jsonResponse');
+      List temp = [];
+      for (var value in jsonResponse['data']) {
+        temp.add(value);
+        // log('temp: ${value['id']}');
+      }
+      return ModelsModel.modelsFromSnapshot(temp);
+    } catch (e) {
+      log('error: $e');
+      rethrow;
+    }
+  }
 }
